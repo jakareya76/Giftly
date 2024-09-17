342a7e48f7f4ca5a8c98af1e4b96a0cd97ec4213
Sahadat Hosen <ssar96540@gmail.com>
2024-09-18 00:22:54 +0600

    Setup react router & folder stucture

---
+++ src/router/Router.jsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from "react-router-dom";
+import {Layout, Home} from "../router/index.js";
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout></Layout>,
+      children:[
+        {
+            path:"/",
+            element:<Home></Home>
+        }
+      ]
+    },
+  ]);
+
+  export default router;
---
+++ src/router/index.js
@@ -0,0 +1,2 @@
+export {default as Layout } from "../layout/Layouts.jsx"
+export {default as Home } from "../pages/home/Home.jsx"
